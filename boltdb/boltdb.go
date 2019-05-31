62ecaf88343a8e870e96df66f16d16b9f9948588
Marius Orcsik <marius@habarnam.ro>
2019-05-31 20:18:08 +0200

    Using go-ap/errors package

--- boltdb/boltdb.go
+++ boltdb/boltdb.go
@@ -1,31 +1,31 @@
 package boltdb
 
 import (
-	"errors"
 	as "github.com/go-ap/activitystreams"
+	"github.com/go-ap/errors"
 	s "github.com/go-ap/storage"
 )
 
 type boltDB struct{}
 
 func (b *boltDB) Load(f s.Filterable) (as.ItemCollection, int, error) {
-	return nil, 0, errors.New("not implemented")
+	return nil, 0, errors.NotImplementedf("BoltDB Load not implemented")
 }
 func (b *boltDB) LoadActivities(f s.Filterable) (as.ItemCollection, int, error) {
-	return nil, 0, errors.New("not implemented")
+	return nil, 0, errors.NotImplementedf("BoltDB LoadActivities not implemented")
 }
 func (b *boltDB) LoadObjects(f s.Filterable) (as.ItemCollection, int, error) {
-	return nil, 0, errors.New("not implemented")
+	return nil, 0, errors.NotImplementedf("BoltDB LoadObjects not implemented")
 }
 func (b *boltDB) LoadCollection(f s.Filterable) (as.CollectionInterface, int, error) {
-	return nil, 0, errors.New("not implemented")
+	return nil, 0, errors.NotImplementedf("BoltDB LoadCollection not implemented")
 }
 func (b *boltDB) SaveActivity(as.Item) (as.Item, error) {
-	return nil, errors.New("not implemented")
+	return nil, errors.NotImplementedf("BoltDB SaveActivity not implemented")
 }
 func (b *boltDB) SaveActor(as.Item) (as.Item, error) {
-	return nil, errors.New("not implemented")
+	return nil, errors.NotImplementedf("BoltDB SaveActor not implemented")
 }
 func (b *boltDB) SaveObject(as.Item) (as.Item, error) {
-	return nil, errors.New("not implemented")
+	return nil, errors.NotImplementedf("BoltDB SaveObject not implemented")
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/go-ap/storage
 
 go 1.12
 
-require github.com/go-ap/activitystreams v0.0.0-20190530184402-5829d059925a
+require (
+	github.com/go-ap/activitystreams v0.0.0-20190530184402-5829d059925a
+	github.com/go-ap/errors v0.0.0-20190531180850-ba5bb7eaf9a5 // indirect
+)
