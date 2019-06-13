99accd64a87723881541d7fbf089be1aefc44c19
Marius Orcsik <marius.orcsik@rocket-internet.de>
2019-06-13 13:30:07 +0200

    Adding placeholder tests

---
+++ boltdb/filters_test.go
@@ -0,0 +1,15 @@
+package boltdb
+
+import "testing"
+
+func TestBoltFilters_ID(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestBoltFilters_IRIs(t *testing.T) {
+	t.Skipf("TODO")
+}
+
+func TestBoltFilters_Types(t *testing.T) {
+	t.Skipf("TODO")
+}
