3d3d09b4b9eedf9626e7d4e308ff839fe798c1fd
Marius Orcsik <marius@habarnam.ro>
2019-08-17 15:26:01 +0200

    Using IRIs array alias for return types of methods

--- filters.go
+++ filters.go
@@ -15,8 +15,8 @@ type Filterable interface {
 
 type FilterableItems interface {
 	Filterable
-	Types() []as.ActivityVocabularyType
-	IRIs() []as.IRI
+	Types() as.ActivityVocabularyTypes
+	IRIs() as.IRIs
 }
 
 // FilterableCollection can filter collections
@@ -27,27 +27,30 @@ type FilterableCollection interface {
 	TotalItemsEq() uint
 	TotalItemsGtE() uint
 	TotalItemsLtE() uint
-	Contains() []as.IRI
+	Contains() as.IRIs
 }
 
 // FilterableActivity can filter activities
 type FilterableActivity interface {
 	FilterableObject
-	Actors() []as.IRI
-	Objects() []as.IRI
-	Targets() []as.IRI
+	Actors() as.IRIs
+	Objects() as.IRIs
+	Targets() as.IRIs
 }
 
 // FilterableObject can filter objects
 type FilterableObject interface {
 	FilterableItems
-	AttributedTo() []as.IRI
-	InReplyTo() []as.IRI
+	AttributedTo() as.IRIs
+	InReplyTo() as.IRIs
 	MediaTypes() []as.MimeType
 	PublishedBefore() time.Time
 	PublishedAfter() time.Time
-	URLs() []as.IRI
+	URLs() as.IRIs
 	// Audience returns the list of IRIs to check against full Audience targeting for the object
 	// It should include all relevant fields: To, CC, BTo, BCC, and Audience
-	Audience() []as.IRI
+	// ---
+	// An element of the Audience is used to get its Inbox end-point and then disseminate the current Activity
+	// to it.
+	Audience() as.IRIs
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/go-ap/storage
 
 go 1.12
 
-require github.com/go-ap/activitystreams v0.0.0-20190706101500-3072014ce9d9
+require github.com/go-ap/activitystreams v0.0.0-20190817131710-a89d5cf5fe18
