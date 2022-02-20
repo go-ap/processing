5d21e1e90b3fc237bc6c0f3f64b487553b7b2638
Marius Orcsik <marius@habarnam.ro>
2022-02-20 19:43:04 +0100

    Added doc for FollowActivity from the AP documentation

--- relationship.go
+++ relationship.go
@@ -43,8 +43,27 @@ func RelationshipManagementActivity(p defaultProcessor, act *pub.Activity) (*pub
 	return act, nil
 }
 
-// FollowActivity
-// is used when following an actor.
+// FollowActivity is used when following an actor.
+// https://www.w3.org/TR/activitypub/#follow-activity-outbox
+// The Follow activity is used to subscribe to the activities of another actor.
+// The side effect of receiving this in an outbox is that the server SHOULD add the object to the actor's following
+// Collection when and only if an Accept activity is subsequently received with this Follow activity as its object.
+// https://www.w3.org/TR/activitypub/#follow-activity-inbox
+// The side effect of receiving this in an inbox is that the server SHOULD generate either an Accept or Reject
+// activity with the Follow as the object and deliver it to the actor of the Follow. The Accept or Reject MAY be
+// generated automatically, or MAY be the result of user input (possibly after some delay in which the user reviews).
+// Servers MAY choose to not explicitly send a Reject in response to a Follow, though implementors ought to be aware
+// that the server sending the request could be left in an intermediate state. For example, a server might not send
+// a Reject to protect a user's privacy.
+// In the case of receiving an Accept referencing this Follow as the object, the server SHOULD add the actor to the
+// object actor's Followers Collection. In the case of a Reject, the server MUST NOT add the actor to the object
+// actor's Followers Collection.
+// NOTE: Sometimes a successful Follow subscription may occur but at some future point delivery to the follower
+// fails for an extended period of time. Implementations should be aware that there is no guarantee that actors on
+// the network will remain reachable and should implement accordingly. For instance, if attempting to deliver to
+// an actor for perhaps six months while the follower remains unreachable, it is reasonable that the delivering
+// server remove the subscriber from the followers list. Timeframes and behavior for dealing with unreachable
+// actors are left to the discretion of the delivering server.
 func FollowActivity(p defaultProcessor, act *pub.Activity) (*pub.Activity, error) {
 	ob := act.Object.GetLink()
 	if !handlers.ValidCollectionIRI(ob) {
