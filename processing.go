4b6653b87e487239c38009b8cd12a63b59214fcb
Marius Orcsik <marius@habarnam.ro>
2022-03-09 15:47:08 +0100

    Don't err on processing Question activities

--- processing.go
+++ processing.go
@@ -374,14 +374,18 @@ func NotificationActivity(l s.WriteStore, act *pub.Activity) (*pub.Activity, err
 }
 
 // QuestionActivity processes matching activities
-// The Questions use case primarily deals with representing inquiries of any type. See 5.4
-// Representing Questions for more information.
+//
+// https://www.w3.org/TR/activitystreams-vocabulary/#h-motivations-questions
+//
+// The Questions use case primarily deals with representing inquiries of any type.
+// See 5.4 Representing Questions for more information: https://www.w3.org/TR/activitystreams-vocabulary/#questions
 func QuestionActivity(l s.WriteStore, act *pub.Question) (*pub.Question, error) {
-	// TODO(marius):
-	return act, errors.NotImplementedf("Processing %s activity is not implemented", act.GetType())
+	// TODO(marius): A Question activity needs to save the objects in the anyOf or oneOf(?) to storage
+	return act, nil
 }
 
 // OffersActivity processes matching activities
+//
 // The Offers use case deals with activities involving offering one object to another. It can include, for instance,
 // activities such as "Company A is offering a discount on purchase of Product Z to Sally",
 // "Sally is offering to add a File to Folder A", etc.
